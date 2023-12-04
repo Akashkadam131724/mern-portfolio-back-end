2c380f37a179986a2b851b9010ed60be14d8365d
Akashkadam131724 <kadamakash131724@gmail.com>
2023-12-04 14:14:26 +0530

    added home route

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ const port = 6002;
 app.use(cors());
 app.use(express.json());
 app.use(router);
+app.get("/", (req, res) => {
+  res.send("<h1>Home routes</h1>");
+});
 
 app.listen(port, () => {
   console.log("Server listening on port", port);
