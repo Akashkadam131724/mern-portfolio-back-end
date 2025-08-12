37e18df43c19482f9f11eb2196d4d22530241130
Akashkadam131724 <kadamakash131724@gmail.com>
2025-08-12 14:49:29 +0530

    Hey

--- Routes/router.js
+++ Routes/router.js
@@ -7,3 +7,7 @@ router.route("/register").post(createUser);
 router.route("/user").get(getUsers);
 
 module.exports = router;
+
+// DATA_BASE =  mongodb+srv://kadamakash131724:iBGYbohzoKtZiLUe@clusterportfolio.bixxduo.mongodb.net/portfolio?retryWrites=true&w=majority
+// EMAIL = kadamakash131724@gmail.com
+// PASSWORD = fvsa noxy qltu ckxd
